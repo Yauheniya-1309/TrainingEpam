5ec38aaa2ba48cacfd389d6ebe4db881e579b3f5
Yauheniya-1309 <43932716+Yauheniya-1309@users.noreply.github.com>
2018-10-22 12:08:56 +0300

    Task 9 (List<T>)

---
+++ List.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Exception.MyExeption;
+
+namespace MYList
+{
+    class MList<T>
+    {
+        const int _LENGTH=10;
+        int _kol;
+        Cell<T> _head;
+
+        public MList()
+        {
+           _head = null;
+        }
+        public MList(T new_val)
+        {
+            Cell<T> new_cell = new Cell<T>(new_val);
+            new_cell._Next = null;
+            _head = new_cell;
+            _kol = 1;
+        }
+
+        public void Push(T new_val)
+        {
+            if(_kol>_LENGTH)
+            {
+                throw  new MyExeption("The List is full");
+            }
+            Cell<T> new_cell = new Cell<T>(new_val);
+            new_cell._Next = _head;
+            _head = new_cell;
+            _kol++;
+        }
+
+        public void Push(T new_val, T pos)
+        {
+            if (_kol > _LENGTH)
+            {
+                throw new MyExeption("The List is full");
+            }
+            bool found = false;
+            Cell<T> temp = _head;
+            while(temp._Next!=null)
+            { if (Object.Equals(temp._Value, pos)) { found = true; break; }
+                else temp = temp._Next;
+            }
+            if (found == true)
+            {
+                Cell<T> new_cell = new Cell<T>(new_val);
+                new_cell._Next = temp._Next;
+                temp._Next = new_cell;
+                _kol++;
+            }
+            else
+            {
+                throw new MyExeption("The value is not exist");
+            }
+        }
+
+       
+        public void Pop()
+        {
+            if(_kol<1)
+            {
+                throw new MyExeption("The list is empty");
+            }
+            _head = _head._Next;
+            _kol--;
+        }
+
+        public void Pop(T del_val)
+        {
+            if (_kol < 1)
+            {
+                throw new MyExeption("The list is empty");
+            }
+            bool found = false; 
+            Cell<T> temp = _head;
+            while (temp._Next != null)
+            {
+                if (Object.Equals(temp._Next._Value, del_val)) { found = true; break; }
+                else temp = temp._Next;
+            }
+            if (found == true)
+            {
+                temp._Next = temp._Next._Next;
+                _kol--;
+            }
+            else
+                
+            {
+                throw new MyExeption("The value is not exist");
+            }
+        }
+
+        public void Show()
+        {
+            Cell<T> temp = _head;
+            while (temp._Next != null)
+            {
+                Console.Write(temp._Value+"  ");
+                temp = temp._Next;
+            }
+            Console.Write(temp._Value + "  ");
+            Console.WriteLine();
+        }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MYList;
+
+namespace MYlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MList<int> int_list = new MList<int>(123);
+            int_list.Show();
+            Console.WriteLine("Список типа int ");
+            try
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    int_list.Push(i);
+                }
+            }
+            catch  (MyExeption ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+                 
+            }
+            int_list.Show();
+            Console.WriteLine("Добавление после несуществующего элемента ");
+            try { int_list.Push(55, 13); }
+            catch  (MyExeption ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            Console.WriteLine("Добавление после существующего элемента ");
+            try { int_list.Push(55, 1); }
+            catch (MyExeption ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            int_list.Show();
+
+            Console.WriteLine("Удаление из конца ");
+            try
+            {
+                int_list.Pop();
+            }
+            catch (MyExeption ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            int_list.Show();
+            Console.WriteLine("Удаление по значению 55");
+            try
+            {
+                int_list.Pop(55);
+            }
+            catch (MyExeption ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            int_list.Show();
+
+
+            Console.WriteLine("Удаление несуществующего элемента ");
+            try
+            {
+                int_list.Pop(33);
+            }
+            catch (MyExeption ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+
+
+            Console.WriteLine("Список типа char ");
+            MList<char> str_list = new MList<char>('A');
+            char a='R'; 
+            for (int i = 0; i < 9; i++)
+            {
+                str_list.Push(a++);
+            }
+            str_list.Show();
+
+            try { str_list.Push('I', 'F'); }
+            catch (MyExeption ex)
+            {
+                Console.WriteLine("Ошибка char: " + ex.Message);
+            }
+            str_list.Show();
+
+            str_list.Pop();
+            str_list.Show();
+
+            str_list.Pop('R');
+            str_list.Show();
+            Console.ReadKey();
+
+        }
+    }
+}
