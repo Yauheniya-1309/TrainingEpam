8921e01726484ed67dfef46799dbe7dbd06bc9de
Yauheniya-1309 <43932716+Yauheniya-1309@users.noreply.github.com>
2018-10-24 10:06:36 +0300

    Task11_FireEvent

---
+++ FireCar1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tas11_FireStation
+{
+    class FireCar1
+    {
+        public void Drive()
+        {
+            Console.WriteLine("I'm car #1!Fire in House");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,100 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using MYList;
 
-namespace MYlist
+namespace Tas11_FireStation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MList<int> int_list = new MList<int>(123);
-            int_list.Show();
-            Console.WriteLine("Список типа int ");
-            try
+          //  bool BigFire;
+            
+            House[] house = new House[5];
+            
+            for (int i = 0; i < house.Length; i++)
             {
-                for (int i = 0; i < 8; i++)
+                house[i] = new House();
+            }
+            
+            FireCar1 car1 = new FireCar1();
+            FireCar2 car2 = new FireCar2();
+
+            Random rnd = new Random();
+           
+            
+         //   
+            for (int i = 0; i < house.Length; i++)
+            {
+                Console.WriteLine("House #{0}", i);
+                
+                int j = rnd.Next(1, 4);
+                
+                if (j == 1)
                 {
-                    int_list.Push(i);
+                    house[i].onFire += car1.Drive;
                 }
-            }
-            catch  (MyExeption ex)
-            {
-                Console.WriteLine("Ошибка: " + ex.Message);
-                 
-            }
-            int_list.Show();
-            Console.WriteLine("Добавление после несуществующего элемента ");
-            try { int_list.Push(55, 13); }
-            catch  (MyExeption ex)
-            {
-                Console.WriteLine("Ошибка: " + ex.Message);
-            }
-            Console.WriteLine("Добавление после существующего элемента ");
-            try { int_list.Push(55, 1); }
-            catch (MyExeption ex)
-            {
-                Console.WriteLine("Ошибка: " + ex.Message);
-            }
-            int_list.Show();
+                else if (j == 2)
+                {
+                    house[i].onFire += car1.Drive;
+                
+                    house[i].onFire += car2.Drive;
+                }
+                else
+                {
+                    Console.WriteLine("No Fire");
 
-            Console.WriteLine("Удаление из конца ");
-            try
-            {
-                int_list.Pop();
+                }
+                house[i].FireInHouse();
+                
+                Console.WriteLine();
             }
-            catch (MyExeption ex)
-            {
-                Console.WriteLine("Ошибка: " + ex.Message);
-            }
-            int_list.Show();
-            Console.WriteLine("Удаление по значению 55");
-            try
-            {
-                int_list.Pop(55);
-            }
-            catch (MyExeption ex)
-            {
-                Console.WriteLine("Ошибка: " + ex.Message);
-            }
-            int_list.Show();
-
-
-            Console.WriteLine("Удаление несуществующего элемента ");
-            try
-            {
-                int_list.Pop(33);
-            }
-            catch (MyExeption ex)
-            {
-                Console.WriteLine("Ошибка: " + ex.Message);
-            }
-
-
-            Console.WriteLine("Список типа char ");
-            MList<char> str_list = new MList<char>('A');
-            char a='R'; 
-            for (int i = 0; i < 9; i++)
-            {
-                str_list.Push(a++);
-            }
-            str_list.Show();
-
-            try { str_list.Push('I', 'F'); }
-            catch (MyExeption ex)
-            {
-                Console.WriteLine("Ошибка char: " + ex.Message);
-            }
-            str_list.Show();
-
-            str_list.Pop();
-            str_list.Show();
-
-            str_list.Pop('R');
-            str_list.Show();
             Console.ReadKey();
-
         }
     }
 }
