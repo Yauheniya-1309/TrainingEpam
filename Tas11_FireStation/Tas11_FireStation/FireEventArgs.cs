77aa2277531d108811a73e3578d1ccba2a341b61
Eugheniya <jane13@mail.ru>
2018-10-24 10:08:11 +0300

    24Oct10:08

---
+++ Tas11_FireStation/Tas11_FireStation/FireEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tas11_FireStation
+{
+    class FireEventArgs:EventArgs
+    {
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        public FireEventArgs()
+            : base()
+        {
+
+        }
+    }
+}
